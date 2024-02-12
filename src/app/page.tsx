bdc4e628ef2e33125f2c43ab85b551b01cc00999
Kei Oikawa <hosikiti@gmail.com>
2024-02-12 13:42:57 +0900

    fix: responsive layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,29 +20,27 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4 dark:bg-black dark:text-white">
-      <div className="text-center">
+      <div className="text-center w-full md:w-[50%]">
         <div className="p-4 flex flex-col gap-4">
           <h1 className="font-bold text-5xl">qrip</h1>
           <p>Share Text with QR code</p>
         </div>
         <div className="flex flex-col gap-4">
-          <div>
-            <div className="flex flex-col items-end">
-              <TextArea
-                value={text}
-                onChange={handleTextChange}
-                placeholder="Enter text to share ..."
-                maxLength={MAX_TEXT_LENGTH}
-              />
-              <CharacterCounter
-                textLength={text.length}
-                maxLength={MAX_TEXT_LENGTH}
-              />
-            </div>
-            <Button onClick={() => copyToClipboard(text)}>
-              Copy to Clipboard
-            </Button>
+          <div className="flex flex-col items-end">
+            <TextArea
+              value={text}
+              onChange={handleTextChange}
+              placeholder="Enter text to share ..."
+              maxLength={MAX_TEXT_LENGTH}
+            />
+            <CharacterCounter
+              textLength={text.length}
+              maxLength={MAX_TEXT_LENGTH}
+            />
           </div>
+          <Button onClick={() => copyToClipboard(text)}>
+            Copy to Clipboard
+          </Button>
           <div className="py-4">
             <h2 className="text-xl font-bold pb-2">Share with this QR code!</h2>
             <div
